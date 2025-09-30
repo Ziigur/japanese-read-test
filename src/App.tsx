abaa1ccc4ec8a599b77e7647e51d319bfbae9fba
Ziigur <zigur94@gmail.com>
2025-09-30 12:35:42 +0300

    Make simple character quessing work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const quiz = [
+  { char: "あ", romaji: "a" },
+  { char: "い", romaji: "i" },
+  { char: "う", romaji: "u" },
+  { char: "え", romaji: "e" },
+  { char: "お", romaji: "o" },
+  { char: "か", romaji: "ka" },
+  { char: "き", romaji: "ki" },
+  { char: "く", romaji: "ku" },
+  { char: "け", romaji: "ke" },
+  { char: "こ", romaji: "ko" },
+  { char: "さ", romaji: "sa" },
+  { char: "し", romaji: "shi" },
+  { char: "す", romaji: "su" },
+  { char: "せ", romaji: "se" },
+  { char: "そ", romaji: "so" },
+  { char: "た", romaji: "ta" },
+  { char: "ち", romaji: "chi" },
+  { char: "つ", romaji: "tsu" },
+  { char: "て", romaji: "te" },
+  { char: "と", romaji: "to" },
+  { char: "な", romaji: "na" },
+  { char: "に", romaji: "ni" },
+  { char: "ぬ", romaji: "nu" },
+  { char: "ね", romaji: "ne" },
+  { char: "の", romaji: "no" },
+  { char: "は", romaji: "ha" },
+  { char: "ひ", romaji: "hi" },
+  { char: "ふ", romaji: "fu" },
+  { char: "へ", romaji: "he" },
+  { char: "ほ", romaji: "ho" },
+  { char: "ま", romaji: "ma" },
+  { char: "み", romaji: "mi" },
+  { char: "む", romaji: "mu" },
+  { char: "め", romaji: "me" },
+  { char: "も", romaji: "mo" },
+  { char: "や", romaji: "ya" },
+  { char: "ゆ", romaji: "yu" },
+  { char: "よ", romaji: "yo" },
+  { char: "ら", romaji: "ra" },
+  { char: "り", romaji: "ri" },
+  { char: "る", romaji: "ru" },
+  { char: "れ", romaji: "re" },
+  { char: "ろ", romaji: "ro" },
+  { char: "わ", romaji: "wa" },
+  { char: "を", romaji: "o" },
+  { char: "ん", romaji: "n" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [text, setText] = useState("");
+  const [question, setQuestion] = useState(quiz[0]);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [inputRef]);
+
+  useEffect(() => {
+    if (text === question.romaji) {
+      // Random next question
+      let nextIndex = Math.floor(Math.random() * quiz.length);
+      while (quiz[nextIndex] === question) {
+        nextIndex = Math.floor(Math.random() * quiz.length);
+      }
+      setQuestion(quiz[nextIndex]);
+      setText("");
+    }
+  }, [text, question]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Write out the character with latin alphabet
       </p>
+      <h1>{question.char}</h1>
+      <div className="card">
+        <input
+          ref={inputRef}
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              if (text === question.romaji) {
+                // Random next question
+                let nextIndex = Math.floor(Math.random() * quiz.length);
+                while (quiz[nextIndex] === question) {
+                  nextIndex = Math.floor(Math.random() * quiz.length);
+                }
+                setQuestion(quiz[nextIndex]);
+                setText("");
+              } else {
+                alert("Wrong answer, try again!");
+                setText("");
+              }
+            }
+          }}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
